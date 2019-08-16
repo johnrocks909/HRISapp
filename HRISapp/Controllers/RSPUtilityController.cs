8c621d3cc9b6d8b8ff89f726912a10b484e615d3
Reden Gallera <rdg577@gmail.com>
2019-08-16 11:06:36 +0800

    RSP Utility

--- HRISapp/Controllers/RSPUtilityController.cs
+++ HRISapp/Controllers/RSPUtilityController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using HRISapp.Models;
@@ -9,7 +10,8 @@ namespace HRISapp.Controllers
 {
     public class RSPUtilityController : Controller
     {
-        HRISDBEntities db = new HRISDBEntities();
+        readonly HRISDBEntities _dbNew = new HRISDBEntities();
+        readonly HRISOldEntities _dbOLD = new HRISOldEntities();
 
         // GET: RSPUtility
         public ActionResult MigrateEmployeeRecords()
@@ -17,6 +19,59 @@ namespace HRISapp.Controllers
             return View();
         }
 
+        /*
+         * PostEmployee
+         * - save employee record to new database
+         */
+        public ActionResult PostEmployee(tRSPEmployee employee)
+        {
+            try
+            {
+                _dbNew.tRSPEmployees.Add(employee);
+                int retval = _dbNew.SaveChanges();
+                if (retval == 0) return new HttpStatusCodeResult(HttpStatusCode.ExpectationFailed, "Sorry, something wrong happen.");
+                return new HttpStatusCodeResult(HttpStatusCode.Created, "New employee is recorded.");
+            }
+            catch (Exception e)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, e.InnerException.Message);
+                //throw;
+            }
+        }
+
+        /*
+         * OldEmployees
+         * - will list down all unmigrated employee records
+         */
+        public ActionResult OldEmployees()
+        {
+            try
+            {
+                var migratedEmployees = _dbNew.tRSPEmployees.Select(t => t.EIC).ToList();
+                var unmigratedEmployees = _dbOLD.tappEmployees.ToList();
+                var filteredUnmigratedEmployees = unmigratedEmployees.Where(u => !migratedEmployees.Contains(u.EIC))
+                                                    .Select(e => new
+                                                    {
+                                                        e.EIC,
+                                                        e.idNo,
+                                                        e.lastName,
+                                                        e.firstName,
+                                                        e.middleName,
+                                                        e.extName,
+                                                        e.birthdate,
+                                                        e.birthplace
+                                                    });
+
+                if (!filteredUnmigratedEmployees.Any()) return null;
+
+                return Json(filteredUnmigratedEmployees.ToList(), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, e.InnerException.Message);
+                //throw;
+            }
+        }
 
     }
 }
